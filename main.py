85761c6d6f7adad0ee2360f6d95675f7ed0a32ae
TranHoNhiThanh <nhithanhtranho@gmail.com>
2020-09-04 15:12:35 +0700

    Init

---
+++ main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import codecs
+import json
+import spacy
+from nltk import sent_tokenize
+import os
+from langdetect import detect
+from utils.utils import preprocess
+
+review_file = os.path.join(os.path.dirname(__file__), 'review.json')
+comment_file = os.path.join(os.path.dirname(__file__), 'comment.json')
+review_output_file = os.path.join(os.path.dirname(__file__), 'review_output.json')
+
+STOP_WORDS = []
+
+
+def get_data(read_file):
+    with open(read_file, encoding="utf8") as f:
+        rs = json.load(f)
+        return [obs.get('content') for obs in rs]
+
+
+def write_result(sentence, entities, preprocess_sentence, language, output_file):
+    dictionary = {
+        "sentence": sentence,
+        "preprocess": preprocess_sentence,
+        "entities": entities,
+        "language": language
+    }
+    json_object = json.dumps(dictionary, indent=4, ensure_ascii=False)
+    with codecs.open(output_file, "a", "utf-8") as f:
+        f.write(json_object)
+        f.write('\n')
+
+
+nlp_vi = spacy.load("vi")
+nlp_en = spacy.load("en_core_web_sm")
+
+review_data = get_data(review_file)
+comment_data = get_data(comment_file)
+
+for txt in review_data:
+    lang = detect(txt)
+    if lang == 'vi':
+        nlp = nlp_vi
+    else:
+        nlp = nlp_en
+    paragraphs = txt.split("\n")
+    for paragraph in paragraphs:
+        for sent in sent_tokenize(paragraph):
+            preprocess_sent = preprocess(sent)
+            doc = nlp(preprocess_sent)
+            ents = []
+            for ent in doc.ents:
+                if hasattr(ent, "label"):
+                    ents.append((ent.text, ent.label_))
+            if ents:
+                write_result(sentence=sent, preprocess_sentence=preprocess_sent, entities=ents, language=lang,
+                             output_file=review_output_file)
---
+++ train_data/ner_train.py
@@ -0,0 +1,110 @@
+TRAIN_DATA = [(
+    'Câu nói ấn tượng nhất với tôi ở tác phẩm của bác Ánh là Nếu bạn đối xử với con chó như một con người nó sẽ đối xử với bạn như một con chó',
+    {'entities': [(49, 52, 'PERSON')]}), ('Nhưng mình sẽ cố gắng nhìn nhận khách quan bé An và Gia',
+                                          {'entities': [(46, 48, 'PERSON'), (52, 55, 'PERSON')]}), (
+    'Ngôn ngữ nhẹ nhàng rất hồn nhiên lắm lúc mình thấy thật trẻ con như đoạn đối thoại giữa các thành viên gđ chị Ni v mình thấy nó thừa và khá nhạt',
+    {'entities': [(110, 112, 'PERSON')]}), (
+    'Nhưng thật lòng mình thấy tác giả như dùng lời kể của chú chó Bê Tô để nói về xã bội loài người cách đối xử tính nết bản chất bla bla',
+    {'entities': [(62, 67, 'PERSON')]}),
+    ('Đôi lúc thích bác Ánh về khoản này', {'entities': [(18, 21, 'PERSON')]}),
+    ('Đánh giá 610 từ Hội An', {'entities': [(16, 22, 'LOC')]}),
+    ('đánh gia mang ý kiến Sài Gòn chủ quan người đọc', {'entities': [(21, 28, 'LOC')]}), (
+        'Một cuốn sách khác viết về động vật Tôi là Bêtô có lẽ đã để lại cho mình rất nhiều ấn tượng nên có lẽ mình đã hơi khó tính với cuốn sách này',
+        {'entities': [(43, 47, 'PERSON')]}), (
+        'Quyển sách đầu tiên mình đọc của tác giả Nguyễn Nhật Ánh là quyển Tôi là Bêto Một quyển sách nói về những suy nghĩ và cách nhìn sự vật qua đôi mắt của một chú chó',
+        {'entities': [(41, 56, 'PERSON'), (73, 77, 'PERSON')]}), (
+        'Quyển Con chó nhỏ mang giỏ hoa hồng của Nguyễn Nhật Ánh chỉ là một cuốn sách có nhưngz câu chuyện cũng do một chú chó kể lại',
+        {'entities': [(40, 55, 'PERSON')]}), (
+        'Vậy nên tác phẩm chưa để lại nhiều ấn tượng cho người đọc đặc biệt ở các thành thị như Sài Gòn, Hà Nội',
+        {'entities': [(87, 94, 'LOC'), (96, 102, 'LOC')]}), (
+        'Trước khi mình đọc cuốn sách này Nhung không phải là người quá yêu chó',
+        {'entities': [(33, 38, 'PERSON')]}), (
+        'Sau khi nghe hết 1 lượt về 5 chú chó cùng với những sinh hoạt hàng ngày với gia đình chị Ni với những người bán hàng hàng xóm đầu ngõ bạn bè anh chị em chú dìnhững chú chó đều thể hiện hết các mặt cá tính của chúng',
+        {'entities': [(89, 91, 'PERSON')]}), (
+        'Buồn cười là từ trước tới giờ cho tới tận khi đọc xong cuốn truyện mình cứ nghĩ chú Ánh là nhà văn nữ Thiệt vậy vì truyện viết trong sáng hồn nhiên quá',
+        {'entities': [(84, 87, 'PERSON')]}),
+    ('Không hiểu sao mà Nguyễn Ngọc Ngạn lại viết được tươi vui đến thế', {'entities': [(18, 34, 'PERSON')]}),
+    ('Nhân vật phụ còn dễ thương hơn nhân vật chính nghe audio mà chán chán kiểu gì from Cà Mau',
+     {'entities': [(83, 89, 'LOC')]}), (
+        'Nhưng tiếc thay cho mối tình của chàng sinh viên xa quê vào Sài Gòn học là Chương và cô láng giềng Quỳnh đã không thể đi đến muôn đời',
+        {'entities': [(60, 67, 'LOC'), (99, 104, 'PERSON'), (75, 81, 'PERSON')]}), (
+        'Về những điểm mình không thích trong truyện Sau khi đọc xong tượng đài về sự dở tệ và đầy mệt mỏi mang tên Mắt biếc mình chính thức xác nhận mình không hợp với văn của bác Ánh tuy vậy mình vẫn đọc vì mình nhớ hồi nhỏ mình rất thích truyện của bác Ánh vả lại sau khi đọc xong một cuốn truyện với nội dung hơi nặng thì mình có xu hướng chọn truyện của bác để đọc giải trí làm nhẹ đầu óc',
+        {'entities': [(107, 115, 'BOOK'), (172, 175, 'PERSON')]}), (
+        'Với mình văn phong của bác ở mức ổn chứ không nổi bật from Minh Anh',
+        {'entities': [(59, 67, 'PERSON')]}), (
+        'Trâm có lẽ là nhân vật mình thấy khá nhất truyện còn lại mình không thích được ai Kim Dung cũng khá ngông nhưng mình không thích bản tánh kiểu vầy',
+        {'entities': [(0, 4, 'PERSON'), (82, 90, 'PERSON')]}), (
+        'Còn chuyện tình giữa Chương và Quỳnh view spoiler Mình không hiểu sao bác Ánh thích viết truyện tình cảm thế đọc truyện này mình thấy gượng ép vô cùng y như tác giả bắt hai người này thích nhau chứ không phải do người này thực sự cảm mến và muốn phát triển tình cảm lên tầm cao hơn với người kia',
+        {'entities': [(21, 27, 'PERSON'), (31, 36, 'PERSON'), (74, 77, 'PERSON')]}), (
+        'Tối ngày cứ chạm mặt tiếng sét ái tình thích nhau thả thính nhau bơ nhau đau khổ dằn vặt chia tay nhau lìa xa nhau lại còn cái màn hát hò thơ thẩn của Chương nữa trời ơi là trời hổng lẽ Chương là phiên bản chưa đủ đô dể hai năm sau bác Ánh tạo ra thằng Ngạn si tình đến khùng điên ba trợn trong Mắt biếc hả',
+        {'entities': [(236, 239, 'PERSON'), (253, 257, 'PERSON'), (295, 303, 'BOOK'), (151, 157, 'PERSON')]}), (
+        'Sao mà mình chán mấy cái trò mèo này thế mà thằng Chương lắm lúc cũng ảo tưởng không kém gì thằng Thư trong Cô gái đến từ hôm qua đọc phát bực cái motif vốn nó đã cũ rích rồi mà cách phát triển tình tiết cũng dở',
+        {'entities': [(50, 56, 'PERSON'), (98, 101, 'PERSON'), (108, 129, 'BOOK')]}), (
+        'đoạn đầu lúc Chương mới lên Sài Gòn và quen với Lan Anh Quỳnh và Trâm đọc ổn một vài đoạn đọc khá hài cho tới khi Chương về quê xong lên thành phố bắt gặp sự thờ ơ lạnh nhạt của Quỳnh thì từ đó về sau nội dung nát bấy đoạn kết dù có khá hơn chút nhưng vẫn không cứu vãn nổi cho toàn bộ cuốn truyện"',
+        {'entities': [(13, 19, 'PERSON'), (28, 35, 'PERSON'), (48, 55, 'PERSON'),
+                      (65, 69, 'PERSON'), (56, 61, 'PERSON')]}), (
+        'Đọc thực sự rất chán Mắt biếc đứng thứ nhất thì đứa nào dám tranh ngôi thứ nhì với cuốn này',
+        {'entities': [(21, 29, 'BOOK')]}), (
+        'và mình đếch quan tâm tại sao Quỳnh bơ Chương luôn không chuyện này thì cũng chuyện kia vốn ngay từ đầu hai đứa thích nhau là cũng sai sai rồi',
+        {'entities': [(30, 35, 'PERSON'), (39, 45, 'PERSON')]}), (
+        'Nam chính trong truyện này trùng tên với bạn cũ mà hiện tại không còn liên lạc nữa của mình nên đọc cũng không thoải mái lắm mà tính cách của thằng này thằng Chương trong truyện á cũng yếu đuối nhu nhược giải quyết không được thì tránh mặt nên mặc dù mình không ghét nhưng vẫn không thể ưa nổi nó bị bỏ là đáng thiệt',
+        {'entities': [(158, 164, 'PERSON')]}), ('Đánh giá 25 from Gia Khánh', {'entities': [(17, 26, 'PERSON')]}),
+    (
+        'Nào là Mèo Gấu beo béo héo úa vì tình nào là Áo Hoa kiêu kì lạnh lùng nào là Tí Hon nhát cáy nào là Út Hoa rụt rè một câu chuyện về tình bạn giữa mèo và chuột tình yêu nồng cháy của mèo Gấu với mèo Áo Hoa thoả thuận sống hoà hợp giữa lũ chuột và kẻ thù của chúng những kế hoạch đánh lạc hướng ông chủ nhà Đọc câu truyện mà tủm tỉm cười mãi nhất là đến đoạn mèo Gấu tan nát trái tim ốm mất hai thánh giời',
+        {'entities': [(7, 14, 'PERSON'), (45, 51, 'PERSON'), (77, 83, 'PERSON'), (100, 106, 'PERSON'),
+                      (194, 204, 'PERSON'), (182, 189, 'PERSON')]}), (
+        'Ước mình có thể quay trở lại thời xưa ấy, nhớ lúc còn học ở Gò Vấp ghê',
+        {'entities': [(60, 66, 'PERSON')]}), (
+        'Mấy nhỏ nhí nhố dễ thương thiệt bàn có 5 chỗ ngồi mà có tới 3 chuyên gia luôn Bảy Huy Quang Hiền Đại mỗi nhỏ một tính cách 1 môn học khác nhau',
+        {'entities': [(78, 81, 'PERSON'), (82, 85, 'PERSON'), (86, 91, 'PERSON'), (92, 96, 'PERSON'),
+                      (97, 100, 'PERSON')]}),
+    (
+        'Câu nói ấn tượng nhất với Nhung ở tác phẩm của bác Nguyễn Nhật Ánh là  Nếu bạn đối xử với con chó như một con người nó sẽ đối xử với bạn như một con chó',
+        {'entities': [(26, 31, 'PERSON'), (51, 66, 'PERSON')]}), (
+        'Quyển sách đầu tiên mình đọc của tác giả Nguyễn Nhật Ánh là quyển Tôi là Bêto  Một quyển sách nói về những suy nghĩ và cách nhìn sự vật qua đôi mắt của một chú chó ở Bình Dương',
+        {'entities': [(166, 176, 'LOC'), (41, 56, 'PERSON'), (73, 77, 'PERSON')]}), (
+        'Trước tiên là nói về những điểm sáng của cuốn sách vẫn là lối hành văn giản dị mộc mạc đầy hóm hỉnh của Nguyễn Nhật ÁNh truyện này viết cũng khá lâu rồi nên cách hành văn tự nhiên chứ không phải kiểu ham lồng ghép vào mấy câu trích dẫn kiểu cố tình tỏ ra sâu sắc nhưng đọc xong quên sạch bách như mấy cuốn sau này mà mình cho là rất phản cảm',
+        {'entities': [(104, 119, 'PERSON')]}), (
+        'Sao mà mình chán mấy cái trò mèo này thế mà thằng Chương lắm lúc cũng ảo tưởng không kém gì thằng Thư trong Cô gái đến từ hôm qua đọc phát bực cái motif vốn nó đã cũ rích rồi mà cách phát triển tình tiết cũng dở',
+        {'entities': [(98, 101, 'PERSON'), (50, 56, 'PERSON')]}), (
+        'Có chút chạnh lòng vì nỗi buồn ở kết truyện nhớ như in câu nói Có hai con mèo ngồi bên cửa sổ 1 con ngồi yên một con đổi chỗ from New Zealand',
+        {'entities': [(130, 141, 'LOC')]}), (
+        'Cái chất Quảng Nam, Quảng Ngãi trong truyện gần gũi nhẹ nhàng và thân thương quá',
+        {'entities': [(20, 30, 'LOC'), (9, 18, 'LOC')]}), (
+        'một câu thơ chân chính cho tình yêu có hai con mèo ngồi bên cửa sổ một con ngồi yên một con đổi chỗ from Sài Gòn',
+        {'entities': [(105, 112, 'LOC')]}), (
+        'Nhàn nhạt êm đềm không chê mà cũng không khen là những điều mình muốn nói về Nữ Sinh',
+        {'entities': [(77, 84, 'BOOK')]}), (
+        'Truyện dài Nữ Sinh của Nguyễn Nhật Ánh giúp cho tâm hồn đọc giả trở nên hồn nhiên và vui tươi như chính các nhân vật trong truyện vậy',
+        {'entities': [(11, 18, 'BOOK'), (23, 38, 'PERSON')]}),
+    (
+    'Sao mà mình chán mấy cái trò mèo này thế, mà thằng Chương lắm lúc cũng ảo tưởng không kém gì thằng Thư trong Cô gái đến từ hôm qua đọc phát bực), cái motif vốn nó đã cũ rích rồi mà cách phát triển tình tiết cũng dở.',
+    {'entities': [(99, 102, 'PERSON'), (109, 130, 'BOOK'), (51, 57, 'PERSON')]}), (
+    'Cả ba vô tình bắt gặp Gia- một anh chàng thư sinh, luôn ngồi uống cà phê tại quán cây Sứ- quán tủ của ba nàng nằm đối diện trường, với đôi mắt xa xăm lạ lùng sau tấm kính thủy tinh.',
+    {'entities': [(22, 25, 'PERSON'), (77, 88, 'LOC')]}), (
+    'Nhưng khi đã già, biết đâu bạn lại cảm thấy: trong đời người, thực ra không có gì gọi là lời đáp cả. – St from Sài Gòn',
+    {'entities': [(103, 105, 'PERSON'), (111, 118, 'LOC')]}), (
+    'Nhớ lúc còn nhỏ cứ canh đến giờ phim Kính vạn hoa để coi, nhưng mãi đến bây giờ mới biết tác giả của kính vạn hoa là Nguyễn Nhật Ánh',
+    {'entities': [(101, 113, 'BOOK'), (117, 132, 'PERSON'), (37, 49, 'BOOK')]}),
+    ('Balo màu xanh là một trong các tập của Kính vạn hoa.', {'entities': [(39, 51, 'BOOK')]}), (
+    'Từ chuyện nhóm Hải Âu vờ đánh cắp ba lô của bộ ba để dẫn các bạn vào trò chơi giải đố, sau đó chiếc ba lô đã bị đánh cắp thật.',
+    {'entities': [(15, 21, 'PERSON')]}), ('TÔI LÀ BÊ TÔ', {'entities': [(7, 12, 'PERSON')]}),
+    ('Nhà phê bình văn học Phạm Xuân Nguyên', {'entities': [(21, 37, 'PERSON')]}), (
+    'Beto và Bino - hai chú cún dễ thương nhìn đời đầy sâu sắc, triết lý',
+    {'entities': [(0, 4, 'PERSON'), (8, 12, 'PERSON')]}), (
+    'Một câu chuyện theo lời kể của một chú chó Bê tô xen lẫn với những cách nhìn về cuộc sống đáng để chúng ta phải suy ngẫm.',
+    {'entities': [(43, 48, 'PERSON')]}), (
+    'Không cần nói nhiều thì cũng biết những điều hay ho mà Bê tô đã mang đến cho mình rồi.',
+    {'entities': [(55, 60, 'PERSON')]}), (
+    'Chú ánh đã rất thành công trong việc để Bê tô nói hết hộ lòng mình những điều chú muốn truyền tới độc giả.',
+    {'entities': [(40, 45, 'PERSON')]}),
+    ('Chắc ai cũng biết vì sao lại là Tôi là Bê tô rồi!', {'entities': [(32, 44, 'BOOK')]}), (
+    'Và không chỉ bê tô, nhà hiền triết Bi nô đã khiến tôi tin chân lí mãi là chân lí, cuộc sông xung quanh có hàng trăm hàng ngàn điều thú vị, để thấy cuộc sống này đáng yêu đáng sống như thế nào.',
+    {'entities': [(13, 18, 'PERSON')]}), (
+    'Gấp lại những trang sách, lòng nhẹ hẳn đi, Văn của Nguyễn Nhật Ánh là thế, đọc xong là xong chứ không có phải suy nghĩ nhiều, suy nghĩ xem tác giả ẩn ý gì thông điệp ra sao, bởi tất cả cần nói đã nói hết rồi, nói súc tích ngắn gọn để ai cũng hiểu rồi mà.',
+    {'entities': [(51, 66, 'PERSON')]}), (
+    'Mỗi lần đọc sách của bác Ánh, tôi nhớ đến ai đó đã nói với tôi rằng Đọc để mà quên, thực sự k phải quên vì nó ám vào tim vào máu rồ thì sao phải nhớ!',
+    {'entities': [(25, 28, 'PERSON')]}), (
+    'Quyển xuất sắc nhất viết về chuyện chó mèo mà mình từng đọc là Nơi trú ẩn của Kathi Appelt, nhưng so với quyển ấy thì Tôi là Bê Tô lại mang đến một cảm giác khác hẳn.',
+    {'entities': [(63, 90, 'BOOK'), (118, 130, 'BOOK')]})
+]
